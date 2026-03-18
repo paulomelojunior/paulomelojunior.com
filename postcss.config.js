5d353aa1f5004753674056d96fa401647f43852d
paulomelojunior <paulomelojunior@icloud.com>
2026-03-18 19:29:59 -0300

    Update Vite configuration to optimize postcss-easing-gradients plugin settings for better gradient rendering

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+import postcssEasingGradients from 'postcss-easing-gradients'
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: [postcssEasingGradients],
+}
+
+export default config
